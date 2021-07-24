e1e63675f7932ba5b30c57197252809ff5b1d67c
Vladimir Bliznyukov <vovablink1@gmail.com>
2021-07-24 22:31:44 +0400

    second practice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('How many films have you watched?', '');
 
 const personalMovieDB = {
@@ -8,7 +10,7 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('The last film you watched', ''),
+/* const a = prompt('The last film you watched', ''),
       b = prompt('How would you rate it?', ''),
       c = prompt('The last film you watched', ''),
       d = prompt('How would you rate it?', '');
@@ -16,5 +18,27 @@ const a = prompt('The last film you watched', ''),
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
+console.log(personalMovieDB); */
+
+//Practice 2
+
+for (let i=0; i<2; i++){
+    const a = prompt('The last film you watched', ''),
+          b = prompt('How would you rate it?', '');
+    if (a == '' || b == '' || a.length > 50 || a == null || b == null){
+       i--;
+    } else {
+       personalMovieDB.movies[a] = b;
+    }
+    
+}
+
 console.log(personalMovieDB);
- 
+
+if (personalMovieDB.count < 10) {
+    console.log("Not that many movies!");
+} else if (10 <= personalMovieDB.count <= 30) {
+    console.log("Not that many movies!");
+} else {
+    console.log("Not that many movies!");
+} 
